8a061c1d80bf3d094ae82dbbc2b9f209d4937347
CAIMEOX <38813005+CAIMEOX@users.noreply.github.com>
2018-08-07 15:21:25 +0800

    Update empty.js

--- empty.js
+++ empty.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 2000 });
 wss.on('connection', function connection(ws) {
 	ws.on("message", function incoming(message) {
 		console.log(message);
